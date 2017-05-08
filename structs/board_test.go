1db9a91d50eef5b31ca958ef9e00629338872eb3
Wei Liao <wliao008@gmail.com>
2017-05-07 21:42:33 -0400

    Added some test cases

--- structs/board_test.go
+++ structs/board_test.go
@@ -31,6 +31,51 @@ func TestNeighbors(t *testing.T) {
 	}
 }
 
+func TestGetDirection(t *testing.T) {
+	b := &Board{10,10,nil}
+	b.Init()
+	var tests = []struct{
+		from *Cell
+		to *Cell
+		want FlagPosition
+	}{
+		{&b.Cells[1][1], &b.Cells[0][1], WEST},
+		{&b.Cells[1][1], &b.Cells[1][0], NORTH},
+		{&b.Cells[1][1], &b.Cells[1][2], SOUTH},
+		{&b.Cells[1][1], &b.Cells[2][1], EAST},
+	}
+
+	for _, test := range tests {
+		dir := b.GetDirection(test.from, test.to)
+		if dir != test.want {
+			t.Errorf("GetDirection(%v, %v), should be %v, got %v", test.from, test.to, dir, test.want)
+		}
+	}
+}
+
+func TestBreakWall(t *testing.T) {
+	b := &Board{10,10,nil}
+	b.Init()
+	var tests = []struct{
+		from, to *Cell
+		dir FlagPosition
+		want1, want2 uint8
+	}{
+		{&b.Cells[1][1], &b.Cells[0][1], WEST, 23, 27},
+		{&b.Cells[1][1], &b.Cells[1][0], NORTH, 30, 29},
+		{&b.Cells[1][1], &b.Cells[1][2], SOUTH, 29, 30},
+		{&b.Cells[1][1], &b.Cells[2][1], EAST, 27, 23},
+	}
+
+	for _, test := range tests {
+		b.BreakWall(test.from, test.to, test.dir)
+		if test.from.Flag != test.want1 && test.to.Flag != test.want2 {
+			t.Error("BreakWall(%v, %v), flags should be %d, %d, got %d, %d", 
+				test.want1, test.want2, test.from.Flag, test.to.Flag)
+		}
+	}
+}
+
 func initBoard() *Board {
 	b := Board{3, 3, nil}
 	b.Init()
