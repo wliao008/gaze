83c1dd1755369f4a8d6d3f0dc4b8bc29a4e10cf4
Wei Liao <wliao008@gmail.com>
2017-05-13 20:01:12 -0400

    tests for algos

--- algos/backtracking_test.go
+++ algos/backtracking_test.go
@@ -1,8 +1,32 @@
 package algos
 
 import (
+	"github.com/wliao008/mazing/structs" 
 	"testing"
 )
+
+func TestBackTrakcingAlgo_Generate_AllCellsVisited(t *testing.T) {
+	k := NewBackTracking(10,10)
+	k.Generate()
+	for i := uint16(0); i < k.Board.Width; i++ {
+		for j := uint16(0); j < k.Board.Height; j++ {
+			if !k.Board.Cells[i][j].IsSet(structs.VISITED) {
+				t.Errorf("Every cell should be visited, but not [%d,%d]", i, j)
+			}
+		}
+	}
+}
+
+func TestNewBackTracking(t *testing.T) {
+	bt := NewBackTracking(10, 10)
+	for _, row := range bt.Board.Cells {
+		for _, cell := range row {
+			if cell.Flag != 15 {
+				t.Errorf("NewBackTrakcing(10,10) should have all flag set to 15, got %d", cell.Flag)
+			}
+		}
+	}
+}
 
 func BenchmarkBackTrackingAlgo50x25(b *testing.B) {
 	for i := 0; i < b.N; i++ {
