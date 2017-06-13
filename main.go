3ced3f7891f4efe285ef6a06e7e1e2e328fad92d
Wei Liao <wliao008@gmail.com>
2017-06-12 21:15:23 -0400

    first try on web

--- main.go
+++ main.go
@@ -2,15 +2,40 @@ package main
 
 import (
 	"fmt"
+	"html/template"
+	"net/http"
 	"github.com/wliao008/mazing/algos"
-	"os"
 )
 
+var tpl *template.Template
+
+func init() {
+	tpl = template.Must(template.ParseGlob("web/templates/*.html"))
+}
+
 func main() {
+	http.HandleFunc("/", index)
+	http.ListenAndServe(":8080", nil)
+}
+
+func index(w http.ResponseWriter, req *http.Request){
 	bt := algos.NewKruskal(40, 20)
 	err := bt.Generate()
 	if err != nil {
 		fmt.Println("ERROR")
 	}
-	bt.Board.Write(os.Stdout)
+	//bt.Board.Write(os.Stdout)
+
+	model := struct {
+		Name string
+		Algo *algos.Kruskal
+	}{
+		"Wei",
+		bt,
+	}
+	err = tpl.ExecuteTemplate(w, "index.html", model)
+	if err != nil {
+		fmt.Println(err)
+		http.Error(w, "Internal server error", http.StatusInternalServerError)
+	}
 }
