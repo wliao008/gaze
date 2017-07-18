d0445f642afe1fdde27f2fdfa44dae62bac10b87
Wei Liao <wliao008@gmail.com>
2017-07-18 16:55:59 -0400

    trying different rendering

--- main.go
+++ main.go
@@ -72,6 +72,7 @@ func getBoard(height, width uint16) (*structs.Board, string) {
 	rand.Seed(time.Now().UTC().UnixNano())
 	var idx int = rand.Intn(3)
 	board := &structs.Board{}
+	idx=0
 	if idx == 0 {
 		k := algos.NewKruskalWeave(height, width)
 		start := time.Now()
@@ -116,6 +117,7 @@ func homeHandler(w http.ResponseWriter, req *http.Request){
 
 	// create model
 	model := &models.BoardModel{}
+	model.TableCss = "cb2"
 	if !strings.Contains(name, "weave") {
 		model.TableCss = "cb"
 	}
@@ -183,11 +185,62 @@ func homeHandler(w http.ResponseWriter, req *http.Request){
 	model.Cells[0][0].CssClasses = strings.Replace(model.Cells[0][0].CssClasses, "north ","",-1)
 	model.Cells[height-1][width-1].CssClasses = strings.Replace(model.Cells[height-1][width-1].CssClasses, "south ","",-1)
 
+
+	model = processWeaveMaze(board, name)
 	err := tpl.ExecuteTemplate(w, "index.tmpl", model)
 	if err != nil {
 		fmt.Println(err)
 		http.Error(w, "Internal server error", http.StatusInternalServerError)
 	}
+}
+
+func processWeaveMaze(board *structs.Board, name string) *models.BoardModel {
+	// create model
+	height := board.Height + (board.Height - 1)
+	width := board.Width + (board.Width - 1)
+	model := &models.BoardModel{}
+	model.TableCss = "cb"
+	model.Name = name
+	model.Height = height
+	model.Width = width
+	model.Cells = make([][]models.CellModel, height)
+	model.RawCells = board.Cells
+	for i := uint16(0); i < height; i++ {
+		model.Cells[i] = make([]models.CellModel, width)
+	}
+
+	// initialize model
+	for h := uint16(0); h < height; h++ {
+		for w := uint16(0); w < width; w++ {
+			model.Cells[h][w].X = w;
+			model.Cells[h][w].Y = h
+			model.Cells[h][w].CssClasses = " north south east west "
+			if h % 2 == 1 || w % 2 == 1 {
+				model.Cells[h][w].CssClasses += "sp "
+			} else {
+				model.Cells[h][w].CssClasses += "td "
+			}
+		}
+	}
+
+	/*
+	for h := uint16(0); h < board.Height; h++ {
+		for w := uint16(0); w < board.Width; w++ {
+			c := board.Cells[h][w]
+			//mcr := model.Cells[h][w*2+1]
+			if !c.IsSet(structs.EAST) {
+				model.Cells[h][w*2].CssClasses += strings.Replace(model.Cells[h][w*2].CssClasses, "east", "", -1)
+				//mcr.CssClasses += strings.Replace(mcr.CssClasses, "west", "", -1)
+				fmt.Println("process")
+			}
+		}
+	}
+	*/
+
+	//set the openning and ending cell
+	model.Cells[0][0].CssClasses = strings.Replace(model.Cells[0][0].CssClasses, "north ","",-1)
+	model.Cells[height-1][width-1].CssClasses = strings.Replace(model.Cells[height-1][width-1].CssClasses, "south ","",-1)
+	return model
 }
 
 func staticHandler(w http.ResponseWriter, req *http.Request) {
